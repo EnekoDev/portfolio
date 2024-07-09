1431819d2f114ba25e8811249d7177925aeb8c04
EnekoDev <enekolabdev@gmail.com>
2024-07-09 10:19:50 +0200

    layout finished

---
+++ src/assets/workExperience.js
@@ -0,0 +1,15 @@
+const arratek = {
+    title: "Desarrollador Web Full Stack",
+    place: "Arratek Informática",
+    timeline: "Actualmente",
+    description: "Diseño, creación y mantenimiento web con CMS WordPress y PrestaShop. Desarrollo webs a medida con JavaScript y PHP. Despliegue y migración web. Configuración de sistemas de mailing para campañas de marketing y eventos mediante SMTP, AcyMailing y Mailchimp. Maquetación de emails. Consultoría tecnológica para PYMES." 
+}
+
+const veltis = {
+    title: "Desarrollador Web Front-End",
+    place: "Veltis Data",
+    timeline: "13/04/2023 - 12/01/2024",
+    description: "Desarrollo Front-End con ReactJS de la plataforma online para clientes. Desarrollo de la UI mediante HTML/CSS, Bootstrap y Ant Design. Implementación de control de login con JWT. Desarrollo de mapas interactivos con Leaflet. Desarrollo de gráficos interactivos con Highcharts. Metodología de trabajo Scrum." 
+}
+
+export { arratek, veltis }
