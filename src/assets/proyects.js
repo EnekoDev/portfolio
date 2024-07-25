14ff0050baadbc97cba24037e45c5c492799be02
EnekoDev <enekolabdev@gmail.com>
2024-07-25 09:01:35 +0200

    proyect and blog entry added

---
+++ src/assets/proyects.js
@@ -0,0 +1,10 @@
+const livechat = {
+    title: "Livechat",
+    link: "https://github.com/EnekoDev/livechat",
+    description: [
+        "App de chat en directo por navegador",
+        "Creada usando Angular, Express y Socket.io"
+    ]
+}
+
+export { livechat }
